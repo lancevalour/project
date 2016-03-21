9c29271a684eabe15c6105fec0688cf87f970f21
ZhangY <zhangyicheng1234@gmail.com>
2016-03-21 18:04:49 -0400

    Add options

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # project
 A nodejs cli to scaffold a project directory. 
+
+# Usage
+```
+$ npm install project
+$ cd [your directory] 
+$ project [project name]
+$ project [project name] [project type]
+```
+
+# Project Type
+android 
+ios
+java 
+
+
+# Todo
--- index.js
+++ index.js
@@ -4,31 +4,34 @@ var fs = require('fs');
 
 var folder = ["./apk", "./code", "./design", "./screenshot", "./test"];
 
+console.log(process.argv);
 console.log(process.argv[2]);
 console.log("Scaffolding...");
 
 var projectName = process.argv[2];
+var options = process.argv[3];
 
-createProjectFolder(projectName);
 
-function createProjectFolder(folderName) {
-    folderName = "./" + folderName;
-    if (!fs.existsSync(folderName)) {
-        fs.mkdirSync(folderName)
-    }
-    try {
-        process.chdir(folderName);
-        console.log('New directory: ' + process.cwd());
-    } catch (err) {
-        console.log('chdir: ' + err);
-    }
-    createSubfolders();
-}
+// createProjectFolder(projectName);
 
-function createSubfolders() {
-    folder.forEach(function(entry) {
-        if (!fs.existsSync(entry)) {
-            fs.mkdirSync(entry);
-        }
-    });
-}
+// function createProjectFolder(folderName) {
+//     folderName = "./" + folderName;
+//     if (!fs.existsSync(folderName)) {
+//         fs.mkdirSync(folderName)
+//     }
+//     try {
+//         process.chdir(folderName);
+//         console.log('New directory: ' + process.cwd());
+//     } catch (err) {
+//         console.log('chdir: ' + err);
+//     }
+//     createSubfolders();
+// }
+
+// function createSubfolders() {
+//     folder.forEach(function(entry) {
+//         if (!fs.existsSync(entry)) {
+//             fs.mkdirSync(entry);
+//         }
+//     });
+// }
