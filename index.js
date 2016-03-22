09dac9cafe9162c4409c28b01decf2e4e17a118b
ZhangY <zhangyicheng1234@gmail.com>
2016-03-22 17:16:32 -0400

    Add more options

--- index.js
+++ index.js
@@ -4,15 +4,73 @@ var fs = require('fs');
 
 var folder = ["./apk", "./code", "./design", "./screenshot", "./test"];
 
-console.log(process.argv);
-console.log(process.argv[2]);
 console.log("Scaffolding...");
 
-var projectName = process.argv[2];
-var options = process.argv[3];
+var argv = ["node", "project", "-type=custom", "folder1", "folder2", "folder3", "-g", "-s", "-a=some", "some1", "some2"];
+
+//var argv = process.argv;
+
+var projectName = argv[2];
+var optionName = argv[3];
+var options = argv.slice(2);
+console.log(options);
+console.log(options.join());
+
+parseOptions(options)
+
+function parseOptions(options){
+    optionList = options.join().split("-");
+
+    console.log(optionList);
+
+    optionList.forEach(function(option) {
+
+      parseOption(option);
+  });
+
+}
+
+function parseOption(option){
+    optionNameValues = option.split(",").filter(function(v){return v!==''});
+
+    if (optionNameValues.length === 0){
+        return;
+    }
+    console.log(optionNameValues);
+    console.log(optionNameValues.length);
 
 
-// createProjectFolder(projectName);
+    optionName = optionNameValues[0];
+    optionValues = optionNameValues.slice(1);
+
+    console.log("optionName: " + optionName);
+    console.log(optionValues);
+
+
+    console.log("optionname 1 " + optionName.split("=")[0]);
+    
+    optionNameValue = optionName.split("=").slice(1);
+
+    
+    switch (optionName.split("=")[0]){
+        case "type":
+        createProjectFolder(projectName);
+        break;
+        case "option2":
+        createProjectFolder(projectName);
+        break;
+        default:
+        console.log("No ");
+        break;
+
+    }
+
+}
+
+function parseType(optionNameValues){
+
+}
+
 
 // function createProjectFolder(folderName) {
 //     folderName = "./" + folderName;
@@ -25,11 +83,12 @@ var options = process.argv[3];
 //     } catch (err) {
 //         console.log('chdir: ' + err);
 //     }
+
 //     createSubfolders();
 // }
 
-// function createSubfolders() {
-//     folder.forEach(function(entry) {
+// function createSubfolders(folderList) {
+//     folderList.forEach(function(entry) {
 //         if (!fs.existsSync(entry)) {
 //             fs.mkdirSync(entry);
 //         }
